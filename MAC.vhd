4fbf7084f7535a29dee8871461f4cf2ad04e10fb
alebal123bal <balzanalessandro2001@gmail.com>
2024-05-08 12:19:47 +0200

    Added MAC logic

--- MAC.vhd
+++ MAC.vhd
@@ -15,7 +15,30 @@ entity MAC is
 end entity MAC;
 
 architecture BHV of MAC is
+    -- Combinational logic signals
     signal mult_s   :   double_data_format;
+    signal sum_s    :   data_format;
+    signal y_s      :   data_format;
+
+    -- Clocked signals out of REG
+    signal sum_s_clkd    :   data_format;
+
     begin
-    
+        CALC_OUT: process(RST, ROM_in, xk_in, READY)
+        begin
+            if RST='0' then
+                mult_s <= (others => '0');
+                sum_s <= (others => '0');
+                y_s <= (others => '0');
+            else
+                mult_s  <=  ROM_in * xk_in;
+                sum_s   <=  mult_s + sum_s_clkd;
+                if READY='0' then
+                    ys <= ys;
+                else
+                    ys <= sum_s_clkd;
+                end if;
+            end if;
+
+        end process CALC_OUT;
     end architecture BHV;
