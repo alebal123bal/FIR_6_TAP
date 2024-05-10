246a74f5d4cf6c0c2a984af2a5811da94635a72c
alebal123bal <balzanalessandro2001@gmail.com>
2024-05-10 12:06:42 +0200

    TODO: correct Buffer bhv

--- SINGLE_CELL.vhd
+++ SINGLE_CELL.vhd
@@ -17,21 +17,21 @@ end entity single_cell;
 
 
 architecture BHV of single_cell is
-    signal xs:  data_format;
+    signal x_s:  data_format;
 
     begin
         -- Input MUX 
-        MUX_2_1:    process(RST, READY, xin, yout, xs)
+        MUX_2_1:    process(RST, READY, xin, yout, x_s)
             begin
                 -- Active-Low Reset
                 if RST='0' then
-                    yout    <= (others => '0');
-                    xs      <= (others => '0');
+                    --yout    <= (others => '0');
+                    x_s      <= (others => '0');
                 else
                     if READY='0' then
-                        xs <= yout;
+                        x_s <= yout;
                     else
-                        xs <= xin;
+                        x_s <= xin;
                     end if;
                 end if;
             end process MUX_2_1;
@@ -40,7 +40,7 @@ architecture BHV of single_cell is
         REG_ASSIGN: process(CLK)
             begin
                 if rising_edge(CLK) then
-                    yout <= xs;
+                    yout <= x_s;
                 end if;
             end process REG_ASSIGN;
 
